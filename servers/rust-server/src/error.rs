0c5c2f5066ae173a3ffede294c1c110ad0eba43d
dfayd0 <78728332+dfayd0@users.noreply.github.com>
2025-09-08 00:56:36 +0200

    feat(market-server-rust): tracing and errors
    Signed-off-by: dfayd0 <78728332+dfayd0@users.noreply.github.com>

---
+++ servers/rust-server/src/error.rs
@@ -0,0 +1,48 @@
+use askama::Template;
+use axum::{
+    http::StatusCode,
+    response::{Html, IntoResponse},
+};
+
+#[derive(displaydoc::Display, pretty_error_debug::Debug, thiserror::Error)]
+pub enum Error {
+    /// Error binding the port to the server
+    Bind(#[source] std::io::Error),
+    /// Error running the server
+    Run(#[source] std::io::Error),
+}
+
+#[derive(Debug, displaydoc::Display, thiserror::Error)]
+pub enum AppError {
+    /// Resource not found
+    NotFound,
+    /// Internal server error
+    Render(#[from] askama::Error),
+    /// Data error
+    Data(#[from] polars::prelude::PolarsError),
+    /// IO error
+    Io(#[from] std::io::Error),
+}
+
+impl IntoResponse for AppError {
+    fn into_response(self) -> axum::response::Response {
+        #[derive(Debug, Template)]
+        #[template(path = "error.html")]
+        struct Tmpl {
+            error: String,
+        }
+
+        let status = match &self {
+            AppError::Render(_) => StatusCode::INTERNAL_SERVER_ERROR,
+            AppError::NotFound => StatusCode::NOT_FOUND,
+            AppError::Data(_) => StatusCode::INTERNAL_SERVER_ERROR,
+            AppError::Io(_) => StatusCode::INTERNAL_SERVER_ERROR,
+        };
+        let tmpl = Tmpl { error: self.to_string() };
+        if let Ok(body) = tmpl.render() {
+            (status, Html(body)).into_response()
+        } else {
+            (status, "Something went wrong").into_response()
+        }
+    }
+}
