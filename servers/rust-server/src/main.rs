efccd16a207281fd60d41a97c33a3a8c3e252313
dfayd0 <78728332+dfayd0@users.noreply.github.com>
2025-09-08 00:56:36 +0200

    feat(main): tracing and errors
    Signed-off-by: dfayd0 <78728332+dfayd0@users.noreply.github.com>

--- servers/rust-server/src/main.rs
+++ servers/rust-server/src/main.rs
@@ -1,38 +1,51 @@
-use axum::{
-    routing::get,
-    routing::post,
-    Router,
-    response::Redirect,
-};
+use axum::{Router, response::Redirect, routing::get, routing::post};
 
+use tower_http::trace::TraceLayer;
+use tracing::Level;
+use tracing::event;
+
+pub mod data;
+pub mod error;
 pub mod home;
+pub mod render;
+
+use crate::error::AppError;
+use crate::error::Error;
 use crate::home::home;
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), Error> {
+    tracing_subscriber::fmt().with_max_level(Level::DEBUG).init();
+
     let addr = "0.0.0.0";
     let port = 4444;
-    let app = Router::new().route("/", get(|| async { Redirect::to("/home") }))
+    let app = Router::new()
+        .route("/", get(|| async { Redirect::to("/home") }))
         .route("/home", get(home))
         .route("/ranking", get(ranking))
         .route("/buy", post(buy))
         .route("/sell", post(sell))
-        .route("/price", get(price));
+        .route("/price", get(price))
+        .fallback(|| async { AppError::NotFound })
+        .layer(TraceLayer::new_for_http());
 
-    let listener = tokio::net::TcpListener::bind(format!("{addr}:{port}")).await.expect("could not bind to address");
-    println!("Listening on http://{addr}:{port}");
-    axum::serve(listener, app).await.expect("could not start server");
+    let listener = tokio::net::TcpListener::bind(format!("{addr}:{port}"))
+        .await
+        .map_err(Error::Bind)?;
+    event!(Level::INFO, "server started and listening on http://{addr}:{port}");
+    axum::serve(listener, app).await.map_err(Error::Run)?;
+    Ok(())
 }
 
-async fn ranking() -> &'static str {
-    "ranking"
+async fn ranking() -> Result<&'static str, AppError> {
+    Ok("ranking")
 }
-async fn buy() -> &'static str {
-    "buy"
+async fn buy() -> Result<&'static str, AppError> {
+    Ok("buy")
 }
-async fn sell() -> &'static str {
-    "sell"
+async fn sell() -> Result<&'static str, AppError> {
+    Ok("sell")
 }
-async fn price() -> &'static str {
-    "price"
+async fn price() -> Result<&'static str, AppError> {
+    Err(AppError::NotFound)
 }
