2d85dfd9c18e234fc6dfb90b8b433227669b2a94
dfayd0 <78728332+dfayd0@users.noreply.github.com>
2025-09-08 00:57:26 +0200

    feat(market-server-rust): separated files, basic templates and a first draft of a home page
    Signed-off-by: dfayd0 <78728332+dfayd0@users.noreply.github.com>

--- servers/rust-server/src/home.rs
+++ servers/rust-server/src/home.rs
@@ -1,51 +1,35 @@
-use axum::{
-    response::{Html},
+use askama::Template;
+use axum::response::{Html, IntoResponse};
+
+use crate::{
+    data::{list_files_in_folder, read_csv},
+    error::AppError,
+    render::plot_candlesticks,
 };
+use rand::seq::SliceRandom;
 
-use plotly::{layout::{themes::BuiltinTheme, Axis}, *};
-use polars::prelude::*;
+type PlotlyHtml = String;
 
-use plotly::{Plot, Layout};
-
-fn read_csv(path: &str) -> Result<DataFrame, PolarsError> {
-    CsvReadOptions::default()
-        .with_has_header(true)
-        .with_parse_options(
-            CsvParseOptions::default().with_try_parse_dates(true),
-        )
-        .try_into_reader_with_file_path(Some(path.into()))?
-        .finish()
+#[derive(Template)]
+#[template(path = "home.html")]
+struct HomeTemplate {
+    plots: Vec<PlotlyHtml>,
 }
 
-fn a_nice_layout(title: &str) -> Layout {
-    let theme = BuiltinTheme::PlotlyDark;
-    let theme_template = theme.build();
-    Layout::new()
-        .title(title)
-        .x_axis(Axis::new().title("Time"))
-        .y_axis(Axis::new().title("Price (USD $)"))
-        .font(plotly::common::Font::new().size(18))
-        .width(1200)
-        .height(800)
-        .template(theme_template)
-}
+pub async fn home() -> Result<impl IntoResponse, AppError> {
+    let mut potentials = list_files_in_folder("./local/data/Stocks")?;
+    let mut rng = rand::rng();
+    potentials.shuffle(&mut rng);
+    let random_files = potentials.into_iter().take(3).collect::<Vec<_>>();
+    let plots: Result<Vec<PlotlyHtml>, AppError> = random_files
+        .iter()
+        .map(|file| {
+            let df = read_csv(&format!("./local/data/Stocks/{}", file))?;
+            let plot = plot_candlesticks(df, Some(&file))?;
+            Ok(plot.to_inline_html(Some(&file)))
+        })
+        .collect();
 
-pub async fn home() -> Html<String> {
-    let df = read_csv("./local/data/Stocks/aapl.us.txt").expect("Failed to read CSV file");
-    
-    let x  = df
-        .column("Date").expect("No 'Date' column")
-        .date().expect("'Date' column is not date").to_string("%Y-%m-%d");
-    let a = x.unwrap().into_no_null_iter().map(|s| s.to_string()).collect::<Vec<_>>();
-    let open = df.column("Open").expect("No 'Open' column").f64().expect("'Open' column is not f64").into_no_null_iter().collect::<Vec<_>>();
-    let high = df.column("High").expect("No 'High' column").f64().expect("'High' column is not f64").into_no_null_iter().collect::<Vec<_>>();
-    let low = df.column("Low").expect("No 'Low' column").f64().expect("'Low' column is not f64").into_no_null_iter().collect::<Vec<_>>();
-    let close = df.column("Close").expect("No 'Close' column").f64().expect("'Close' column is not f64").into_no_null_iter().collect::<Vec<_>>();
-    // println!("{:?}", df);
-
-    let trace = Candlestick::new(a, open, high, low, close);
-    let mut plot = Plot::new();
-    plot.add_trace(trace);
-    plot.set_layout(a_nice_layout("AAPL Stock Prices"));
-    Html(plot.to_html())
+    let template = HomeTemplate { plots: plots? };
+    Ok(Html(template.render()?))
 }
