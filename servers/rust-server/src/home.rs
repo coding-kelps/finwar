1624ddc6c61ba8c4da5d4b90cb327e15805a6583
dfayd0 <78728332+dfayd0@users.noreply.github.com>
2025-09-09 06:17:09 +0200

    feat(home): a draft of an historgram. not sure what we're looking at.
    Signed-off-by: dfayd0 <78728332+dfayd0@users.noreply.github.com>

--- servers/rust-server/src/home.rs
+++ servers/rust-server/src/home.rs
@@ -1,10 +1,11 @@
 use askama::Template;
 use axum::response::{Html, IntoResponse};
+use plotly::histogram;
 
 use crate::{
     data::{list_files_in_folder, read_csv},
     error::AppError,
-    render::{plot_candlesticks, plot_pie_files},
+    render::{plot_candlesticks, plot_histogram, plot_pie_files},
 };
 use rand::seq::SliceRandom;
 
@@ -22,15 +23,26 @@ pub async fn home() -> Result<impl IntoResponse, AppError> {
     let mut rng = rand::rng();
     potentials.shuffle(&mut rng);
     let random_files = potentials.into_iter().take(3).collect::<Vec<_>>();
-    let mut plots: Vec<PlotlyHtml> = random_files
+
+    let dataframes = random_files
         .iter()
-        .map(|file| {
-            let df = read_csv(&format!("./local/data/Stocks/{}", file))?;
-            let plot = plot_candlesticks(df, Some(&file))?;
+        .map(|file| read_csv(&format!("./local/data/Stocks/{}", file)))
+        .collect::<Result<Vec<_>, _>>()?;
+
+    let histogram = plot_histogram(&dataframes, &random_files)?;
+
+    let mut plots: Vec<PlotlyHtml> = dataframes
+        .iter()
+        .zip(random_files.iter())
+        .map(|(df, file)| {
+            let plot = plot_candlesticks(df.clone(), Some(&file))?;
             Ok(plot.to_inline_html(Some(&file)))
         })
         .collect::<Result<_, AppError>>()?;
+
     plots.insert(0, plot_pie.to_inline_html(Some("distribution")));
+    plots.insert(1, histogram.to_inline_html(Some("histogram")));
+
     let template = HomeTemplate { plots };
     Ok(Html(template.render()?))
 }
