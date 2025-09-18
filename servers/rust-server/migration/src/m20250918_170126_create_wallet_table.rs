f6f800b3929d5fa09028769d0bc04b07f18b58dc
dfayd0 <78728332+dfayd0@users.noreply.github.com>
2025-09-18 22:59:31 +0200

    feat(market-server-rust): sea-orm set up
    Signed-off-by: dfayd0 <78728332+dfayd0@users.noreply.github.com>

---
+++ servers/rust-server/migration/src/m20250918_170126_create_wallet_table.rs
@@ -0,0 +1,48 @@
+use crate::m20250918_163000_create_bots_table::Bot;
+use sea_orm_migration::{prelude::*, schema::*};
+
+#[derive(DeriveMigrationName)]
+pub struct Migration;
+
+#[async_trait::async_trait]
+impl MigrationTrait for Migration {
+    async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .create_table(
+                Table::create()
+                    .table(Wallet::Table)
+                    .if_not_exists()
+                    .col(pk_auto(Wallet::Id))
+                    .col(integer(Wallet::BotId))
+                    .col(decimal_len(Wallet::Cash, 15, 2))
+                    .col(decimal_len(Wallet::Asset, 5, 2))
+                    .col(timestamp_with_time_zone(Wallet::CreatedAt))
+                    .col(timestamp_with_time_zone(Wallet::UpdatedAt))
+                    .foreign_key(
+                        ForeignKey::create()
+                            .name("fk-wallet-bot_id")
+                            .from(Wallet::Table, Wallet::BotId)
+                            .to(Bot::Table, Bot::Id)
+                            .on_delete(ForeignKeyAction::Cascade)
+                            .on_update(ForeignKeyAction::Cascade),
+                    )
+                    .to_owned(),
+            )
+            .await
+    }
+
+    async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager.drop_table(Table::drop().table(Wallet::Table).to_owned()).await
+    }
+}
+
+#[derive(DeriveIden)]
+enum Wallet {
+    Table,
+    Id,
+    BotId,
+    Cash,
+    Asset,
+    CreatedAt,
+    UpdatedAt,
+}
