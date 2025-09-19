a7664b2504addee6216d58696fb2785e6ff6693e
dfayd0 <78728332+dfayd0@users.noreply.github.com>
2025-09-19 12:13:01 +0200

    feat(rust-server): simple axum server (#3)
    * feat(rust-server): simple axum server
    
    Signed-off-by: dfayd0 <78728332+dfayd0@users.noreply.github.com>
    
    * fix(routing) sell and buy are now in post methods
    
    Signed-off-by: dfayd0 <78728332+dfayd0@users.noreply.github.com>
    
    * refactor(cargo): renaming package name
    
    Signed-off-by: dfayd0 <78728332+dfayd0@users.noreply.github.com>
    
    * docs(cargo): package metadata
    
    Signed-off-by: dfayd0 <78728332+dfayd0@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: dfayd0 <78728332+dfayd0@users.noreply.github.com>

---
+++ servers/rust-server/src/main.rs
@@ -0,0 +1,33 @@
+use axum::{
+    routing::get,
+    routing::post,
+    Router,
+};
+
+#[tokio::main]
+async fn main() {
+    let addr = "0.0.0.0";
+    let port = 4444;
+    let app = Router::new().route("/", get(|| async { "Welcome to FinWar!" }))
+        .route("/ranking", get(ranking))
+        .route("/buy", post(buy))
+        .route("/sell", post(sell))
+        .route("/price", get(price));
+
+    let listener = tokio::net::TcpListener::bind(format!("{addr}:{port}")).await.expect("could not bind to address");
+    println!("Listening on http://{addr}:{port}");
+    axum::serve(listener, app).await.expect("could not start server");
+}
+
+async fn ranking() -> &'static str {
+    "ranking"
+}
+async fn buy() -> &'static str {
+    "buy"
+}
+async fn sell() -> &'static str {
+    "sell"
+}
+async fn price() -> &'static str {
+    "price"
+}
