059c7592d818e70b2be4a4baf0107f9768c02b4e
dfayd0 <78728332+dfayd0@users.noreply.github.com>
2025-09-25 12:16:48 +0200

    feat(market-server-rust): a begining of data sanitazition on trade routes
    Signed-off-by: dfayd0 <78728332+dfayd0@users.noreply.github.com>

--- servers/rust-server/entity/wallet.rs
+++ servers/rust-server/entity/wallet.rs
@@ -8,7 +8,8 @@ pub struct Model {
     #[sea_orm(primary_key)]
     pub id: i32,
     pub bot_id: i32,
-    pub cash: i32,
+    #[sea_orm(column_type = "Decimal(Some((5, 2)))")]
+    pub cash: Decimal,
     #[sea_orm(column_type = "Decimal(Some((5, 2)))")]
     pub asset: Decimal,
     pub created_at: DateTimeWithTimeZone,
