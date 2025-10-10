b10e2b5ea327ba6f630786394ecd0a9968305aef
dfayd0 <78728332+dfayd0@users.noreply.github.com>
2025-10-10 14:49:28 +0200

    refactor(market-server-rust): remove Plotly and Polars dependencies, clean up home template

--- servers/rust-server/src/state.rs
+++ servers/rust-server/src/state.rs
@@ -1,9 +1,3 @@
-use crate::{
-    data::{list_files_in_folder, read_csv},
-    render::{PlotlyHtml, plot_pie_files},
-};
-use polars::frame::DataFrame;
-use rand::seq::SliceRandom;
 use sea_orm::DatabaseConnection;
 use std::fmt;
 
@@ -30,10 +24,6 @@ impl std::error::Error for StateError {}
 
 #[derive(Clone)]
 pub struct AppState {
-    pub all_files: Vec<String>,
-    pub randos: Vec<String>,
-    pub dataframes: Vec<DataFrame>,
-    pub pie_distrubtion: PlotlyHtml,
     pub db: sea_orm::DatabaseConnection,
     pub uuid_prefix_length: usize,
     pub starting_cash: f64,
@@ -42,26 +32,7 @@ pub struct AppState {
 
 impl AppState {
     pub async fn new(db: DatabaseConnection) -> Result<Self, StateError> {
-        let mut all_files = list_files_in_folder("./local/data/Stocks")
-            .map_err(|_| StateError::InitState)?;
-        let plot_pie = plot_pie_files(&all_files)
-            .map_err(|_| StateError::InitState)?
-            .to_inline_html(Some("file size distribution"));
-        let mut rng = rand::rng();
-        all_files.shuffle(&mut rng);
-        let randos = all_files.clone().into_iter().take(3).collect::<Vec<_>>();
-
-        let dataframes = randos
-            .iter()
-            .map(|file| read_csv(&format!("./local/data/Stocks/{}", file)))
-            .collect::<Result<Vec<_>, _>>()
-            .map_err(|_| StateError::InitState)?;
-
         Ok(AppState {
-            all_files,
-            randos,
-            dataframes,
-            pie_distrubtion: plot_pie,
             db,
             uuid_prefix_length: 18,
             starting_cash: 10000.0,
